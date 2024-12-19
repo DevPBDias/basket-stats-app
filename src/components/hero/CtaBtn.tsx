003e9d6742f5af9b597b8ac71c4b4a3b8614a653
DevPBDias <devpbdias@gmail.com>
2024-12-19 17:03:06 -0300

    fix: add hover effect to cta btn

--- src/components/hero/CtaBtn.tsx
+++ src/components/hero/CtaBtn.tsx
@@ -6,7 +6,7 @@ const CtaBtn = () => {
   return (
     <Link
       href="/game"
-      className="md:max-w-64 flex flex-row w-full lg:w-60 justify-center items-center gap-3 bg-[#F26419] rounded-xl py-[6px] lg:py-2 shadow-lg mt-5"
+      className="md:max-w-64 flex flex-row w-full lg:w-60 justify-center items-center gap-3 bg-[#F26419] hover:scale-105 hover:border-2 hover:border-[#0B1D51] rounded-xl py-[6px] lg:py-2 shadow-lg mt-5"
     >
       <p className="text-[#0B1D51] font-bold lg:text-lg">Ir para o jogo</p>
       <ArrowRightCircle size={24} color="#0B1D51" />
